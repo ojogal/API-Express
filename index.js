efae680e8717899a413317ab551dba3b2d57caef
ojogal <ojog.al@gmail.com>
2023-01-20 21:48:02 +0200

    Router updated

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import * as http from 'node:http';
-import EventEmitter from 'node:events';
+import Router from './router.js'
 
 const PORT = process.env.PORT || 4001;
-const emitter = new EventEmitter();
 
 const server = http.createServer((req, res) => {
   try {
@@ -15,33 +14,7 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, () => { console.log(`Server started on PORT ${PORT}`) })
 
-class Router {
-  constructor() {
-    this.endpoints = {}
-  };
 
-  request(method = 'GET', path, handler) {
-    if (!this.endpoints[path]) {
-      this.endpoints[path] = {}
-    };
-
-    // /users [GET, POST, PUT], /post [GET, POST, PUT, DELETE]
-    const endpoint = this.endpoints[path];
-
-    if (endpoint[method]) { throw Error(`${method} method with ${path} address alreay exitsts`) };
-
-    endpoint[method] = handler;
-    emitter.on(`[${path}]:[${method}]`, (req, res) => { handler(req, res) })
-  };
-
-  get(path, handler) { this.request('GET', path, handler) };
-
-  post(path, handler) { this.request('POST', path, handler) };
-
-  put(path, handler) { this.request('PUT', path, handler) };
-
-  delete(path, handler) { this.request('DELETE', path, handler) };
-};
 
 const router = new Router();
 
---
+++ router.js
@@ -0,0 +1,31 @@
+import EventEmitter from 'node:events';
+
+const emitter = new EventEmitter();
+
+export default class Router {
+  constructor() {
+    this.endpoints = {}
+  };
+
+  request(method = 'GET', path, handler) {
+    if (!this.endpoints[path]) {
+      this.endpoints[path] = {}
+    };
+
+    // /users [GET, POST, PUT], /post [GET, POST, PUT, DELETE]
+    const endpoint = this.endpoints[path];
+
+    if (endpoint[method]) { throw Error(`${method} method with ${path} address alreay exitsts`) };
+
+    endpoint[method] = handler;
+    emitter.on(`[${path}]:[${method}]`, (req, res) => { handler(req, res) })
+  };
+
+  get(path, handler) { this.request('GET', path, handler) };
+
+  post(path, handler) { this.request('POST', path, handler) };
+
+  put(path, handler) { this.request('PUT', path, handler) };
+
+  delete(path, handler) { this.request('DELETE', path, handler) };
+};
